a67c003a6be7436d86de9550e3780f1ca2adfc49
gitfasen <panchenko.a91@gmail.com>
2017-01-13 22:15:37 +0200

    add jquery.countTo

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,11 @@
-$(function() {
-
-	// Custom JS
-
+jQuery(function ($) {
+      // custom formatting example
+      $('.timer').countTo({
+	    from: 0,
+	    to: 2500,
+	    speed: 5000,
+	    formatter: function (value, options) {
+	      return value.toFixed(options.decimals);
+	    },
+	  });
 });
